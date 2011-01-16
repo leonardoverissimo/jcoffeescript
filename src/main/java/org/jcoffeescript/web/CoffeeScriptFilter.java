770040590c76d0bd1730b9d6f7a4b7c59decba34
Leonardo Veríssimo da Silva <leonardo@leonardo-Inspiron-1525.(none)>
2011-01-16 12:18:53 -0200

    Adding web filter that compiles on the fly

---
+++ src/main/java/org/jcoffeescript/web/CoffeeScriptFilter.java
@@ -0,0 +1,99 @@
+package org.jcoffeescript.web;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.jcoffeescript.JCoffeeScriptCompileException;
+import org.jcoffeescript.JCoffeeScriptCompiler;
+
+public class CoffeeScriptFilter implements Filter {
+
+	private static final int BUFFER_SIZE = 4096;
+	
+
+	@Override
+	public void init(FilterConfig config) throws ServletException {
+		JCoffeeScriptCompiler compiler = new JCoffeeScriptCompiler();
+		config.getServletContext().setAttribute("compiler", compiler);
+	}
+	
+	
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+			throws IOException, ServletException {
+		
+		HttpServletRequest request = (HttpServletRequest) req;
+//		HttpServletResponse response = (HttpServletResponse) resp;
+		
+		JCoffeeScriptCompiler compiler = (JCoffeeScriptCompiler) request.getSession()
+				.getServletContext().getAttribute("compiler");
+
+		String coffeeFilename = discoverCoffeeFilename(request.getPathInfo());
+		InputStream coffeeStream = request.getSession().getServletContext().getResourceAsStream(coffeeFilename);
+		if (coffeeStream == null) {
+			// pass just to show 404 default page
+			chain.doFilter(req, resp);
+			return;
+		}
+		
+		String source = getContent(coffeeStream);
+
+		try {
+			String binary = compiler.compile(source);
+
+			resp.setContentType("text/javascript");
+			PrintWriter writer = resp.getWriter();
+			writer.write(binary);
+			
+		} catch (JCoffeeScriptCompileException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private String discoverCoffeeFilename(String javascriptResource) {
+
+		Pattern pattern = Pattern.compile("/js/(.*).js");
+		Matcher matcher = pattern.matcher(javascriptResource);
+		if (!matcher.matches()) {
+			return null;
+		}
+		return "/WEB-INF/coffee/" + matcher.group(1) + ".coffee";
+	}
+
+	private String getContent(InputStream coffeeStream) throws IOException {
+		InputStreamReader reader = new InputStreamReader(coffeeStream);
+		try {
+			StringWriter writer = new StringWriter();
+
+			char[] buffer = new char[BUFFER_SIZE];
+			int n = 0;
+			while (-1 != (n = reader.read(buffer))) {
+				writer.write(buffer, 0, n);
+			}
+			String source = writer.toString();
+			return source;
+		} finally {
+			reader.close();
+		}
+	}
+
+}
---
+++ src/unit-tests/java/org/jcoffeescript/CoffeeScriptWebFilterTest.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2011 Leonardo Verissimo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jcoffeescript;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.nio.SelectChannelConnector;
+import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.servlet.FilterMapping;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.jcoffeescript.web.CoffeeScriptFilter;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CoffeeScriptWebFilterTest {
+	
+	@BeforeClass
+	public static void setUp() throws Exception {
+		Server server = new Server();
+		SelectChannelConnector connector = new SelectChannelConnector();
+        connector.setPort(9012);
+        server.addConnector(connector);
+		
+		// setting context
+		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+		context.addFilter(CoffeeScriptFilter.class, "*.js", FilterMapping.DEFAULT);
+		context.addServlet(DefaultServlet.class, "/*");
+		context.setResourceBase("target/classes/unit-tests/app");
+        
+        server.setHandler(context);
+        
+		server.start();
+	}
+	
+	@Test
+	public void shouldReturnCompiledJSResource() throws Exception {
+		
+		HttpClient httpClient = new HttpClient();
+		
+		GetMethod method = new GetMethod("http://localhost:9012/js/simple.js");
+		int response = httpClient.executeMethod(method);
+		
+		assertEquals(200, response);
+		assertTrue(method.getResponseBodyAsString().startsWith("(function() {\n"));
+		assertTrue(method.getResponseBodyAsString().endsWith("\n}).call(this);\n"));
+	}
+	
+	@Test
+	public void shouldReturn404() throws Exception {
+		
+		HttpClient httpClient = new HttpClient();
+		
+		GetMethod method = new GetMethod("http://localhost:9012/js/notfound.js");
+		int response = httpClient.executeMethod(method);
+		
+		assertEquals(404, response);
+//		System.out.println(method.getResponseBodyAsString());
+	}
+}
